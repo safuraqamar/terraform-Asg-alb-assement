a33aef9ffec635202d3399533b03a6fed6c2fb5c
emad <emadsyed02@gmail.com>
2021-10-18 23:29:13 -0400

    Adding availability zone as per doc

--- resources/subnets/subnets_private.tf
+++ resources/subnets/subnets_private.tf
@@ -1,5 +1,6 @@
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = var.vpc_id
+  availability_zone = ["us-east-1a", "us-east-1c"]
   cidr_block        = "10.1.2.0/24"
     tags = {
     Name = "${var.name}_Private_1"
@@ -7,6 +8,7 @@ resource "aws_subnet" "private_subnet_1" {
 }
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = var.vpc_id
+  availability_zone = ["us-east-1a", "us-east-1c"]
   cidr_block        = "10.1.3.0/24"
    tags = {
     Name = "${var.name}_Private_2"
--- resources/subnets/subnets_public.tf
+++ resources/subnets/subnets_public.tf
@@ -1,5 +1,6 @@
 resource "aws_subnet" "public_subnet_1" {
   vpc_id            = var.vpc_id
+  availability_zone = ["us-east-1a", "us-east-1c"]
   cidr_block        = "10.1.0.0/24"
     tags = {
     Name = "${var.name}_Public_1"
@@ -8,6 +9,7 @@ resource "aws_subnet" "public_subnet_1" {
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id            = var.vpc_id
+  availability_zone = ["us-east-1a", "us-east-1c"]
   cidr_block        = "10.1.1.0/24"
   
   tags = {
